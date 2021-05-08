97a56cfbc21bc61b9c04bd2846c5d64941fd1219
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-05-08 14:53:44 -0500

    errorHandler, notFoundHandler and validationHandler implemented

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ const app = express();
 const { config } = require('./config');
 const moviesAPI = require('./routes/movies');
 
+const {
+    logErrors,
+    wrapError,
+    errorHandler,
+} = require('./utils/middleware/errorHandlers');
+
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
+
+// body parser
 app.use(express.json());
 
 moviesAPI(app);
+
+// Catch 404
+app.use(notFoundHandler);
+
+// Errors middleware
+app.use(logErrors);
+app.use(wrapError);
+app.use(errorHandler);
 
 app.listen(config.port, () => {
     console.log(`Listening http://localhost:${config.port}`);
