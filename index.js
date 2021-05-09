a8722f59abf71371efe2120dedf2bad3b8b1c191
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-05-09 13:49:35 -0500

    validations in movie endpoints

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 const { config } = require('./config');
@@ -11,6 +12,9 @@ const {
 } = require('./utils/middleware/errorHandlers');
 
 const notFoundHandler = require('./utils/middleware/notFoundHandler');
+
+// morgan
+app.use(morgan(config.dev ? 'dev' : 'common'));
 
 // body parser
 app.use(express.json());
@@ -26,5 +30,6 @@ app.use(wrapError);
 app.use(errorHandler);
 
 app.listen(config.port, () => {
+    // eslint-disable-next-line no-console
     console.log(`Listening http://localhost:${config.port}`);
 });
--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const MoviesService = require('../services/movies');
 
+const {
+    movieIdSchema,
+    createMovieSchema,
+    updateMovieSchema,
+} = require('../schemas/movies');
+
+const validationHandler = require('../utils/middleware/validationHandler');
+
 function moviesAPI(app) {
     const router = express.Router();
     app.use('/api/movies', router);
@@ -17,62 +25,85 @@ function moviesAPI(app) {
         }
     });
 
-    router.get('/:movieId', async function (req, res, next) {
-        const { movieId } = req.params;
-        try {
-            const retrievedMovie = await moviesService.getMovie({ movieId });
-            res.status(200).json({
-                data: retrievedMovie,
-                messages: 'movie retrieved',
-            });
-        } catch (error) {
-            next(error);
+    router.get(
+        '/:movieId',
+        validationHandler({ movieId: movieIdSchema }, 'params'),
+        async function (req, res, next) {
+            const { movieId } = req.params;
+            try {
+                const retrievedMovie = await moviesService.getMovie({
+                    movieId,
+                });
+                res.status(200).json({
+                    data: retrievedMovie,
+                    messages: 'movie retrieved',
+                });
+            } catch (error) {
+                next(error);
+            }
         }
-    });
+    );
 
-    router.post('/', async function (req, res, next) {
-        const { body: movie } = req;
-        try {
-            const createdMovieId = await moviesService.createMovie({ movie });
-            res.status(201).json({
-                data: createdMovieId,
-                messages: 'movie created',
-            });
-        } catch (error) {
-            next(error);
+    router.post(
+        '/',
+        validationHandler(createMovieSchema),
+        async function (req, res, next) {
+            const { body: movie } = req;
+            try {
+                const createdMovieId = await moviesService.createMovie({
+                    movie,
+                });
+                res.status(201).json({
+                    data: createdMovieId,
+                    messages: 'movie created',
+                });
+            } catch (error) {
+                next(error);
+            }
         }
-    });
+    );
 
-    router.put('/:movieId', async function (req, res, next) {
-        const { movieId } = req.params;
-        const { body: movie } = req;
-        try {
-            const updatedMovieId = await moviesService.updateMovie({
-                movieId,
-                movie,
-            });
+    router.put(
+        '/:movieId',
+        validationHandler({ movieId: movieIdSchema }, 'params'),
+        validationHandler(updateMovieSchema),
+        async function (req, res, next) {
+            const { movieId } = req.params;
+            const { body: movie } = req;
+            try {
+                const updatedMovieId = await moviesService.updateMovie({
+                    movieId,
+                    movie,
+                });
 
-            res.status(200).json({
-                data: updatedMovieId,
-                messages: 'movie updated',
-            });
-        } catch (error) {
-            next(error);
+                res.status(200).json({
+                    data: updatedMovieId,
+                    messages: 'movie updated',
+                });
+            } catch (error) {
+                next(error);
+            }
         }
-    });
+    );
 
-    router.delete('/:movieId', async function (req, res, next) {
-        const { movieId } = req.params;
-        try {
-            const deletedMovieId = await moviesService.deleteMovie({ movieId });
-            res.status(200).json({
-                data: deletedMovieId,
-                messages: 'movie deleted',
-            });
-        } catch (error) {
-            next(error);
+    router.delete(
+        '/:movieId',
+        validationHandler({ movieId: movieIdSchema }, 'params'),
+        async function (req, res, next) {
+            const { movieId } = req.params;
+            try {
+                const deletedMovieId = await moviesService.deleteMovie({
+                    movieId,
+                });
+                res.status(200).json({
+                    data: deletedMovieId,
+                    messages: 'movie deleted',
+                });
+            } catch (error) {
+                next(error);
+            }
         }
-    });
+    );
 }
 
 module.exports = moviesAPI;
